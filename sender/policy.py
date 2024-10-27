678641e69c0d01c780147a9278acfe2f96ef2594
Samuele Marro <marrosamuele@gmail.com>
2024-10-27 17:12:17 +0000

    Added simple policy.

---
+++ sender/policy.py
@@ -0,0 +1,11 @@
+class SimpleSenderPolicy:
+    def __init__(self, memory, num_conversations_for_protocol, num_conversations_for_implementation):
+        self.memory = memory
+        self.num_conversations_for_protocol = num_conversations_for_protocol
+        self.num_conversations_for_implementation = num_conversations_for_implementation
+    
+    def should_negotiate_protocol(self, task_id, task_data, target):
+        return self.memory.get_num_conversations(task_id, target) >= self.num_conversations_for_protocol
+    
+    def should_implement_protocol(self, task_id, task_data, target):
+        return self.memory.get_num_conversations(task_id, target) >= self.num_conversations_for_implementation
