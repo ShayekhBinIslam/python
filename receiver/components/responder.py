5537552e73da28f78655766e0a4dfcd4b7e05c77
Samuele Marro <marrosamuele@gmail.com>
2024-12-07 20:48:20 +0000

    Added basic multiround support for receiver.

---
+++ receiver/components/responder.py
@@ -0,0 +1,54 @@
+# The responder is a special toolformer that replies to a service based on a protocol document.
+# It receives the protocol document and writes the response that must be sent to the system.
+
+import json
+import os
+from pathlib import Path
+
+from toolformers.base import Toolformer
+
+from toolformers.base import Conversation
+from common.storage import Storage
+
+# TODO: A tool to declare an error?
+
+
+PROTOCOL_RESPONDER_PROMPT = 'You are ResponderGPT. Below you will find a document describing detailing how to respond to a query. '\
+    'The communication might involve multiple rounds of back-and-forth.' \
+    'Use the provided functions to execute what is requested and provide the response according to the protocol\'s specification. ' \
+    'Only reply with the response itself, with no additional information or escaping. Similarly, do not add any additional whitespace or formatting.'# \
+   # 'If you do not have enough information to reply, or if you cannot execute the request, reply with "ERROR" (without quotes).'
+
+NL_RESPONDER_PROMPT = 'You are NaturalLanguageResponderGPT. You will receive a query from a user. ' \
+    'Use the provided functions to execute what is requested and reply with a response (in natural language). ' \
+    'Important: the user does not have the capacity to respond to follow-up questions, so if you think you have enough information to reply/execute the actions, do so.'
+    #'If you do not have enough information to reply, if you cannot execute the request, or if the request is invalid, reply with "ERROR" (without quotes).' \
+
+class Responder:
+    def __init__(self, toolformer : Toolformer, tools, additional_info : str):
+        self.toolformer = toolformer
+        self.tools = tools
+        self.additional_info = additional_info
+
+    def create_protocol_conversation(self, protocol_document):
+        print('===NL RESPONDER (WITH PROTOCOL)===')
+
+        prompt = PROTOCOL_RESPONDER_PROMPT + self.additional_info + '\n\nThe protocol is the following:\n\n' + protocol_document
+
+        return self.toolformer.new_conversation(prompt, self.tools, category='conversation')
+
+
+    def create_nl_conversation(self):
+        print('===NL RESPONDER (NO PROTOCOL)===')
+        print(NL_RESPONDER_PROMPT + self.additional_info)
+
+        print('Preparing NL response with tools:', [tool.name for tool in self.tools])
+
+        conversation = self.toolformer.new_conversation(NL_RESPONDER_PROMPT + self.additional_info, self.tools, category='conversation')
+        return conversation
+
+    def create_conversation(self, protocol_document):
+        if protocol_document is None:
+            return self.create_nl_conversation()
+        else:
+            return self.create_protocol_conversation(protocol_document)
--- receiver/core.py
+++ receiver/core.py
@@ -1,57 +1,36 @@
+from abc import ABC, abstractmethod
 from typing import List
 
-from receiver.responder import Responder
-from receiver.storage import ReceiverStorage, LocalReceiverStorage
-from utils import download_and_verify_protocol
-
-from common.core import Suitability
 from toolformers.base import Tool
+from common.storage import Storage, JSONStorage
+from receiver.components.responder import Responder
+from receiver.components.protocol_checker import ReceiverProtocolChecker
+
 
 class Receiver:
-    def __init__(self, memory: ReceiverStorage, responder : Responder, protocol_checker : ReceiverProtocolChecker, tools: List[Tool]):
-        self.memory = memory
+    def __init__(self, storage : Storage, responder : Responder, protocol_checker : ReceiverProtocolChecker, tools: List[Tool], additional_info : str = ''):
+        self.storage = storage
         self.responder = responder
         self.protocol_checker = protocol_checker
         self.tools = tools
+        self.additional_info = additional_info
 
-    def handle_query(self, protocol_hash, protocol_sources, body):
-        if protocol_hash is None:
-            return self.responder.reply_to_nl_query(body)
-            
-        if self.memory.has_implementation(protocol_hash):
-            routine_path = self.memory.get_implementation_path(protocol_hash)
-            return self.executor.run_routine(routine_path, body)
-        else:
-            self.memory.increment_conversations(protocol_hash)
+    @staticmethod
+    def make_default(toolformer, storage : Storage = None, responder : Responder = None, protocol_checker : ReceiverProtocolChecker = None, tools : List[Tool] = None, additional_info : str = ''):
+        if tools is None:
+            tools = []
 
-        if not self.memory.has_protocol_document(protocol_hash):
-            for protocol_source in protocol_sources:
-                protocol_document = download_and_verify_protocol(protocol_hash, protocol_source)
-                if protocol_document is not None:
-                    self.memory.register_new_protocol(protocol_hash, protocol_source, protocol_document)
-                    break
-            else:
-                return {
-                    'status': 'error',
-                    'message': 'No valid protocol source provided.'
-                }
+        if storage is None:
+            path = './receiver_storage.json'
+            storage = JSONStorage(path) # TODO
 
-        if self.memory.get_suitability(protocol_hash) == Suitability.UNKNOWN:
-            protocol_document = self.memory.load_protocol_document(protocol_hash)
-            is_suitable = self.protocol_checker.check_protocol_for_tools(protocol_document, self.tools)
-            if is_suitable:
-                self.memory.set_suitability(protocol_hash, Suitability.ADEQUATE)
-            else:
-                self.memory.set_suitability(protocol_hash, Suitability.INADEQUATE)
-            self.memory.save_memory()
-        
-        if self.memory.get_suitability(protocol_hash) == Suitability.ADEQUATE:
-            if self.policy.should_implement_protocol(protocol_hash):
-                implementation = self.programmer.write_routine_for_task(protocol_hash)
-                self.memory.register_implementation(protocol_hash, implementation)
-            return self.executor.execute_task(protocol_hash, body)
-        else:
-            return {
-                'status': 'error',
-                'message': 'Protocol not suitable.'
-            }
+        if responder is None:
+            responder = Responder(toolformer, tools, additional_info)
+
+        if protocol_checker is None:
+            protocol_checker = ReceiverProtocolChecker()
+
+        return Receiver(storage, responder, protocol_checker, tools, additional_info)
+
+    def create_conversation(self, protocol_hash, protocol_sources):
+        return self.responder.create_conversation(None)
