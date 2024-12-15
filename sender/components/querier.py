a4780245c11ac072099422aaabb69407b084e3d4
Samuele Marro <marrosamuele@gmail.com>
2024-12-15 20:26:10 +0000

    Improved querier.

--- sender/components/querier.py
+++ sender/components/querier.py
@@ -2,19 +2,23 @@
 # It receives the protocol document and writes the query that must be performed to the system.
 
 import json
-import os
-from pathlib import Path
-
 import sys
+from typing import Callable, Dict
 
-from toolformers.base import Tool, StringParameter, parameter_from_openai_api
+from toolformers.base import Tool, Toolformer, StringParameter, parameter_from_openai_api
 
-from utils import send_raw_query
+PROTOCOL_QUERIER_PROMPT = 'You are NaturalLanguageQuerierGPT. You act as an intermediary between a machine (who has a very specific input and output schema) and an external service (which follows a very specific protocol).' \
+    'You will receive a task description (including a schema of the input and output that the machine uses) and the corresponding data. Call the \"sendQuery\" tool with a message following the protocol.' \
+    'Do not worry about managing communication, everything is already set up for you. Just focus on sending the right message.' \
+    'The sendQuery tool will return the reply of the service.\n' \
+    'Some protocols may explictly require multiple queries. In that case, you can call sendQuery multiple times. Otherwise, call it only once. \n' \
+    'In any case, you cannot call sendQuery more than {max_queries} time(s), no matter what the protocol says.' \
+    'Once you receive the reply, call the "deliverStructuredOutput" tool with parameters according to the task\'s output schema. \n' \
+    'You cannot call deliverStructuredOutput multiple times, so make sure to deliver the right output the first time.' \
+    'If there is an error and the machine\'s input/output schema specifies how to handle an error, return the error in that format. Otherwise, call the "error" tool.' \
+    #'Also, if the query fails to .'
 
-PROTOCOL_QUERIER_PROMPT = 'You are QuerierGPT. You will receive a protocol document detailing how to query a service. Reply with a structured query which can be sent to the service.' \
-    'Only reply with the query itself, with no additional information or escaping. Similarly, do not add any additional whitespace or formatting.'
-
-def construct_query_description(protocol_document, task_schema, task_data):
+def construct_query_description(protocol_document : str, task_schema, task_data):
     query_description = ''
     if protocol_document is not None:
         query_description += 'Protocol document:\n\n'
@@ -29,17 +33,17 @@ def construct_query_description(protocol_document, task_schema, task_data):
 
     return query_description
 
-NL_QUERIER_PROMPT = 'You are NaturalLanguageQuerierGPT. You act as an intermediary between a machine (who has a very specific input and output schema) and an agent (who uses natural language).' \
-    'You will receive a task description (including a schema of the input and output) that the machine uses and the corresponding data. Call the \"sendQuery\" tool with a natural language message where you ask to perform the task according to the data.' \
+NL_QUERIER_PROMPT = 'You are NaturalLanguageQuerierGPT. You act as an intermediary between a machine (which has a very specific input and output schema) and an agent (who uses natural language).' \
+    'You will receive a task description (including a schema of the input and output that the machine uses) and the corresponding data. Call the \"sendQuery\" tool with a natural language message where you ask to perform the task according to the data.' \
     'Make sure to mention all the relevant information. ' \
     'Do not worry about managing communication, everything is already set up for you. Just focus on asking the right question.' \
     'The sendQuery tool will return the reply of the service.\n' \
-    'Once you receive the reply, call the \"deliverStructuredOutput\" tool with parameters according to the task\'s output schema. \n' \
-    'Note: you cannot call sendQuery multiple times, so make sure to ask the right question the first time. Similarly, you cannot call deliverStructuredOutput multiple times, so make sure to deliver the right output the first time.' \
-    'If the query fails, do not attempt to send another query.'
+    'Once you have enough information, call the \"deliverStructuredOutput\" tool with parameters according to the task\'s output schema. \n' \
+    'Note: you can only call sendQuery {max_queries} time(s), so be efficient. Similarly, you cannot call deliverStructuredOutput multiple times, so make sure to deliver the right output the first time.' \
+    'If there is an error and the machine\'s input/output schema specifies how to handle it, return the error in that format. Otherwise, call the "error" tool.'
+    #'If the query fails, do not attempt to send another query.'
 
-def parse_and_handle_query(query, callback):
-    # TODO: Replace with transporter
+def parse_and_handle_query(query, callback : Callable[[str], Dict]):
     try:
         response = callback(query)
 
@@ -48,6 +52,8 @@ def parse_and_handle_query(query, callback):
         else:
             return 'Error calling the tool: ' + response['message']
     except Exception as e:
+        import traceback
+        traceback.print_exc()
         return 'Error calling the tool: ' + str(e)
 
 def get_output_parameters(task_schema):
@@ -63,28 +69,33 @@ def get_output_parameters(task_schema):
     return parameters
 
 class Querier:
-    def __init__(self, toolformer):
+    def __init__(self, toolformer : Toolformer, max_queries : int = 5, max_messages : int = None):
         self.toolformer = toolformer
+        self.max_queries = max_queries
 
-    def handle_conversation(self, prompt, message, output_parameters, callback):
-        sent_query_counter = 0
-        
+        if max_messages is None:
+            max_messages = max_queries * 2
+
+        self.max_messages = max_messages
+
+    def handle_conversation(self, prompt : str, message : str, output_parameters, callback):
+        query_counter = 0
+
         def send_query_internal(query):
             print('Sending query:', query)
-            nonlocal sent_query_counter
-            sent_query_counter += 1
+            nonlocal query_counter
+            query_counter += 1
 
-            if sent_query_counter > 50:
+            if query_counter > self.max_queries + 10:
                 # All hope is lost, crash
                 sys.exit(-2)
-            elif sent_query_counter > 10:
+            elif query_counter > self.max_queries + 5:
                 # LLM is not listening, throw an exception
                 raise Exception('Too many attempts to send queries. Exiting.')
-            elif sent_query_counter > 5:
+            elif query_counter > self.max_queries:
                 # LLM is not listening, issue a warning
                 return 'You have attempted to send too many queries. Finish the message and allow the user to speak, or the system will crash.'
-            elif sent_query_counter > 1:
-                return 'You have already sent a query. You cannot send another one.'
+
             return parse_and_handle_query(query, callback)
 
         send_query_tool = Tool('sendQuery', 'Send a query to the other service based on a protocol document.', [
@@ -92,6 +103,7 @@ class Querier:
         ], send_query_internal)
 
         found_output = None
+        found_error = None
         registered_output_counter = 0
 
         def register_output(**kwargs):
@@ -102,7 +114,7 @@ class Querier:
             if found_output is not None:
                 registered_output_counter += 1
 
-            if registered_output_counter > 50:
+            if registered_output_counter > 20:
                 # All hope is lost, crash
                 sys.exit(-2)
             elif registered_output_counter > 10:
@@ -123,16 +135,31 @@ class Querier:
             output_parameters
         , register_output)
 
-        conversation = self.toolformer.new_conversation(prompt, [send_query_tool, register_output_tool], category='conversation')
+        def register_error(error):
+            nonlocal found_error
+            found_error = error
+            return 'Error registered. Finish the message and allow the user to speak.'
 
-        for i in range(5):
+        error_tool = Tool('error', 'Return an error message to the machine.', [
+            StringParameter('error', 'The error message to return to the machine', True)
+        ], register_error)
+
+        prompt = prompt.format(max_queries=self.max_queries)
+
+        conversation = self.toolformer.new_conversation(prompt, [send_query_tool, register_output_tool, error_tool], category='conversation')
+
+        # TODO: Make number of attempts configurable
+        for _ in range(self.max_messages):
             conversation(message, print_output=True)
+
+            if found_error is not None:
+                raise Exception('Error:', found_error)
 
             if found_output is not None:
                 break
 
             # If we haven't sent a query yet, we can't proceed
-            if sent_query_counter == 0:
+            if query_counter == 0:
                 message = 'You must send a query before delivering the structured output.'
             elif found_output is None:
                 message = 'You must deliver the structured output.'
@@ -144,24 +171,3 @@ class Querier:
         output_parameters = get_output_parameters(task_schema)
 
         return self.handle_conversation(PROTOCOL_QUERIER_PROMPT, query_description, output_parameters, callback)
-
-
-    #def send_query_with_protocol(self, storage, task_schema, task_data, target_node, protocol_id, source):
-    #    base_folder = Path(os.environ.get('STORAGE_PATH')) / 'protocol_documents'
-    #    protocol_document = storage.load_protocol_document(base_folder, protocol_id)
-    #    query_description = construct_query_description(protocol_document, task_schema, task_data)
-    #    output_parameters = get_output_parameters(task_schema)
-#
-    #    return self.handle_conversation(PROTOCOL_QUERIER_PROMPT, query_description, target_node, protocol_id, source, output_parameters)
-#
-    #def send_query_without_protocol(self, task_schema, task_data, target_node):
-    #    query_description = construct_query_description(None, task_schema, task_data)
-    #    output_parameters = get_output_parameters(task_schema)
-#
-    #    return self.handle_conversation(NL_QUERIER_PROMPT, query_description, target_node, None, None, output_parameters)
-    #
-    #def send_query(self, storage, task_schema, task_data, target_node, protocol_id, source):
-    #    if protocol_id is not None:
-    #        return self.send_query_with_protocol(storage, task_schema, task_data, target_node, protocol_id, source)
-    #    else:
-    #        return self.send_query_without_protocol(task_schema, task_data, target_node)
