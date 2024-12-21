f19d4108111af96928059ca4a7fe37216e89ed5b
Samuele Marro <marrosamuele@gmail.com>
2024-12-21 16:50:10 +0000

    Extended JSONStorage.

--- common/storage.py
+++ common/storage.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
+from collections.abc import MutableMapping
 import json
 from pathlib import Path
 
-class Storage(ABC):
+class Storage(ABC, MutableMapping):
     @abstractmethod
     def save_memory(self):
         pass
@@ -10,15 +11,7 @@ class Storage(ABC):
     @abstractmethod
     def load_memory(self):
         pass
-  
 
-    @abstractmethod
-    def get(self, key):
-        pass
-
-    @abstractmethod
-    def set(self, key, value):
-        pass
 
 class JSONStorage(Storage):
     def __init__(self, storage_path, autosave=True):
@@ -29,18 +22,38 @@ class JSONStorage(Storage):
         self.autosave = autosave
 
     def save_memory(self):
-        with open(self.storage_path / 'memory.json', 'w') as f:
-            json.dump(self.data, f)
+        with open(self.storage_path, 'w') as f:
+            json.dump(self.data, f, indent=2)
 
     def load_memory(self):
-        with open(self.storage_path / 'memory.json', 'r') as f:
+        if not self.storage_path.exists():
+            self.save_memory()
+        with open(self.storage_path, 'r') as f:
             self.data = json.load(f)
 
-    def get(self, key):
+    def __getitem__(self, key):
         return self.data.get(key)
 
-    def set(self, key, value):
+    def __setitem__(self, key, value):
         self.data[key] = value
 
         if self.autosave:
             self.save_memory()
+
+    def __delitem__(self, key):
+        del self.data[key]
+
+        if self.autosave:
+            self.save_memory()
+    
+    def __iter__(self):
+        return iter(self.data)
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __contains__(self, key):
+        return key in self.data
+    
+    def __repr__(self):
+        return f'JSONStorage({self.storage_path})'
