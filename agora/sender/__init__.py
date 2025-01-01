9b44d1880203c5660b616d236fe83e0481fe67e6
Samuele Marro <marrosamuele@gmail.com>
2025-01-01 23:28:51 +0000

    Simplified imports.

---
+++ agora/sender/__init__.py
@@ -0,0 +1,7 @@
+from agora.sender.core import Sender, SenderMemory
+
+import agora.sender.components.negotiator as negotiator
+import agora.sender.components.programmer as programmer
+import agora.sender.components.protocol_picker as protocol_picker
+import agora.sender.components.querier as querier
+import agora.sender.components.transporter as transporter
