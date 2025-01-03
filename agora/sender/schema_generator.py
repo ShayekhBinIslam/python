57fe7ade1800472dc298d3f5ca42846becc2d9b5
Samuele Marro <marrosamuele@gmail.com>
2025-01-03 11:42:58 +0000

    Added override support to SchemaGenerator.

--- agora/sender/schema_generator.py
+++ agora/sender/schema_generator.py
@@ -36,7 +36,21 @@ class SchemaGenerator:
         """
         self.toolformer = toolformer
     
-    def _generate(self, prompt: str, message: str) -> TaskSchema:
+    def _generate(self, prompt: str, message: str, description: str = None, input_schema: dict = None, output_schema: dict = None) -> TaskSchema:
+        if description is not None and input_schema is None and output_schema is None:
+            # We can generate the schema directly
+            return TaskSchema(description, input_schema, output_schema)
+
+        # We inform the toolformer of the overrides, since they can be useful to generate the rest of the schema
+        if description is not None:
+            message += '\n\n' + 'Description override:\n\n' + description
+
+        if input_schema is not None:
+            message += '\n\n' + 'Input schema override:\n\n' + json.dumps(input_schema, indent=2)
+
+        if output_schema is not None:
+            message += '\n\n' + 'Output schema override:\n\n' + json.dumps(output_schema, indent=2)
+
         conversation = self.toolformer.new_conversation(prompt, [], category='schema')
 
         reply = conversation(message, print_output=True)
@@ -46,15 +60,27 @@ class SchemaGenerator:
 
         schema = json.loads(schema)
 
+        if description is not None:
+            schema['description'] = description
+        
+        if input_schema is not None:
+            schema['input_schema'] = input_schema
+
+        if output_schema is not None:
+            schema['output_schema'] = output_schema
+
         return TaskSchema.from_json(schema)
 
 
-    def from_function(self, func: Callable) -> TaskSchema:
+    def from_function(self, func: Callable, description: str = None, input_schema: dict = None, output_schema: dict = None) -> TaskSchema:
         """Generate a TaskSchema schema from a function.
         Unlike TaskSchema.from_function, this method supports generating schemas from functions without type hints.
 
         Args:
             func (Callable): The function to generate the schema from.
+            description (str, optional): If not None, overrides the generated description. Defaults to None.
+            input_schema (dict, optional): If not None, overrides the generated input schema. Defaults to None.
+            output_schema (dict, optional): If not None, overrides the generated output schema. Defaults to None.
 
         Returns:
             TaskSchema: The generated schema.
@@ -63,19 +89,22 @@ class SchemaGenerator:
 
         message = 'Function code:\n\n' + inspect.getsource(func)
 
-        return self._generate(prompt, message)
+        return self._generate(prompt, message, description=description, input_schema=input_schema, output_schema=output_schema)
 
-    def from_description(self, description: str) -> TaskSchema:
-        """Generate a JSON schema from a description.
+    def from_text(self, text: str, description: str = None, input_schema: dict = None, output_schema: dict = None) -> TaskSchema:
+        """Generate a JSON schema from a textual description.
 
         Args:
-            description (str): The description of the function.
+            text (str): The description of the task.
+            description (str, optional): If not None, overrides the generated description. Defaults to None.
+            input_schema (dict, optional): If not None, overrides the generated input schema. Defaults to None.
+            output_schema (dict, optional): If not None, overrides the generated output schema. Defaults to None.
 
         Returns:
             TaskSchema: The generated schema.
         """
         prompt = SCHEMA_GENERATOR_PROMPT.format(EXTRA_RULES='')
 
-        message = 'Description:\n\n' + description
+        message = 'Description of the task:\n\n' + text
 
-        return self._generate(prompt, message)
+        return self._generate(prompt, message, description=description, input_schema=input_schema, output_schema=output_schema)
